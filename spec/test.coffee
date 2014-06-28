241e3401073760db394d95c4c329a38b4c9696d2
Starkhorn <t.earendel@gmail.com>
2014-06-28 18:49:46 +0700

    Made Karma run tests written in coffee script

---
+++ spec/test.coffee
@@ -0,0 +1,3 @@
+describe 'sanity test with coffeescript', ->
+    it '1 + 1 should be 2', ->
+        expect(1 + 1).toEqual 2
--- spec/test.js
+++
@@ -1,7 +0,0 @@
-describe('sanity test', function() {
-
-    it('1 + 1 should be 2', function() {
-        expect(1 + 1).toEqual(2);
-    });
-
-});
